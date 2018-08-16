e8ad75df04aa80f42c01cc3bfca3bbe4007a04d9
Anmol Agrawal <anmol1771@gmail.com>
2018-08-16 21:40:40 +0530

    basic ext working

--- background.js
+++ background.js
@@ -1,45 +1,9 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log("alert from background.js");
+  chrome.tabs.executeScript({file: "jquery-3.1.1.min.js"}, function() {
+      console.log("jquery Loaded");
   });
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  //   chrome.declarativeContent.onPageChanged.addRules([{
-  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-  //       pageUrl: {hostEquals: 'bbc.com'},
-  //     })
-  //     ],
-  //         actions: [new chrome.declarativeContent.ShowPageAction()]
-  //   }]);
-  // });
+  chrome.tabs.executeScript({file: "popup.js"}, function() {
+      console.log("content loaded");
+  });
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   let trybutton = document.getElementById('try');
-//   trybutton.on('click', function() {
-//     console.log("yo")
-//     let article_text = $("#page > div:nth-child(1) > div.container > div > div.column--primary > div.story-body > div.story-body__inner")[0]
-//     console.log(article_text)
-//   })
-// })
-
-// $( document ).ready(function() {
-//   console.log( "ready!" );
-//   let trybutton = $('#try');
-//   trybutton.click(() =>{
-//     let article_text = $("#page > div:nth-child(1) > div.container > div > div.column--primary > div.story-body > div.story-body__inner")[0]
-//     console.log(article_text)
-//   })
-// });
-
-// chrome.extension.onRequest.addListener(function() {
-//   let trybutton = document.getElementById('try');
-//   trybutton.on('click', function() {
-//     console.log("yo")
-//   //   let article_text = $("#page > div:nth-child(1) > div.container > div > div.column--primary > div.story-body > div.story-body__inner")[0]
-//   //   console.log(article_text)
-//   })
-// });
-
-$('#try').click(function () {
-  console.log("yo")
-})
--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-  $('#try').click(function () {
-    console.log("try")
-  })
-})
+var ans = {};
+let tryButton = document.getElementById('try');
+
+ans.createSidebar = function() {
+
+    return {
+        init: function(){
+            chrome.extension.getBackgroundPage();
+            tryButton.onclick = function(element) {
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {code: 'alert(document.querySelector("h1").textContent);'});
+              });
+            };
+        }
+    }
+}();
+
+ans.createSidebar.init();
